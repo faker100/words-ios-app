97bed69b29d8454fd6e855e5013dd6384a53b3d9
Charles <2485539514@qq.com>
2018-02-24 18:09:35 +0800

    no message

---
+++ Word/Main/ReciteWords/Index/View/LGIndexReviewAlertView.h
@@ -0,0 +1,13 @@
+//
+//  LGIndexReviewAlertView.h
+//  Word
+//
+//  Created by Charles Cao on 2018/2/24.
+//  Copyright © 2018年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGIndexReviewAlertView : UIView
+
+@end
---
+++ Word/Main/ReciteWords/Index/View/LGIndexReviewAlertView.m
@@ -0,0 +1,21 @@
+//
+//  LGIndexReviewAlertView.m
+//  Word
+//
+//  Created by Charles Cao on 2018/2/24.
+//  Copyright © 2018年 Charles. All rights reserved.
+//
+
+#import "LGIndexReviewAlertView.h"
+
+@implementation LGIndexReviewAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
